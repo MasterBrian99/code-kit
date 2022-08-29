83550534dab6fcf6a366b2af49b3e1e54da47ad7
pasindu p konghawaththa <37585474+MasterBrian99@users.noreply.github.com>
2022-08-29 23:17:00 +0530

    initial commit

---
+++ code-kit-web/src/main.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+
+declare global {
+  interface Window {
+    Neutralino: any;
+  }
+}
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+window.Neutralino.init();
